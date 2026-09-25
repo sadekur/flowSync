e252ec1018f418c97c5df8647dd5f4ef90d812dd
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-09-25 22:56:54 +0600

    feat: update hooks to use typed dispatch and selector for improved type safety

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,6 @@
-import { useDispatch, useSelector, useStore, type TypedUseSelectorHook } from "react-redux";
+import { useDispatch, useSelector, useStore } from "react-redux";
 import type { AppDispatch, AppStore, RootState } from "./index";
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppStore: () => AppStore = useStore;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppStore = useStore.withTypes<AppStore>();
