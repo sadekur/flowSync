ee1c17b9eb17d3698e0139eda87250c76721fc05
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-09-25 22:53:36 +0600

    feat: add messages reducer to store configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,6 +11,7 @@ export function makeStore() {
     reducer: {
       auth: authReducer,
       socket: socketReducer,
+      messages: messagesReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(createSocketMiddleware()),
   });
