2c4a97d43aeb3e3b4762d21fd3f9246a87ab54d9
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-09-25 22:49:10 +0600

    feat: import Message model in project service for project-related operations

--- backend/src/services/project.service.ts
+++ backend/src/services/project.service.ts
@@ -1,5 +1,6 @@
 import { Project, type ProjectDocument } from "../models/Project";
 import { Task } from "../models/Task";
+import { Message } from "../models/Message";
 import type { WorkspaceDocument } from "../models/Workspace";
 import { generateUniqueSlug } from "../utils/slug";
 import type { CreateProjectInput, UpdateProjectInput } from "../validators/project.validators";
