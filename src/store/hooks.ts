583ef3e41cdab1f4072ff25dc86b5e23a1f5a43e
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-09-25 22:53:54 +0600

    feat: add useAppStore hook to access the Redux store

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,5 +1,6 @@
-import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux";
-import type { AppDispatch, RootState } from "./index";
+import { useDispatch, useSelector, useStore, type TypedUseSelectorHook } from "react-redux";
+import type { AppDispatch, AppStore, RootState } from "./index";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppStore: () => AppStore = useStore;
